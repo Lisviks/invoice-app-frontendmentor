efe99b6d4ccac3fe07234c92ab702395afa2a12a
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-07-13 20:09:58 +0100

    css custom properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Header from './components/Header';
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-      <h1>Invoice app</h1>
-    </main>
+      <main></main>
+    </>
   );
 }
