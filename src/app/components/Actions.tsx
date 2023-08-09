0d16611efbe5a801d9244bd236b3ba606d7db4d1
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-08-09 06:02:03 +0100

    update total invoices number on Action component

--- src/app/components/Actions.tsx
+++ src/app/components/Actions.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image';
 import ArrowIcon from '@/assets/icon-arrow-down.svg';
 import PlusIcon from '@/assets/icon-plus.svg';
 import styles from '@/app/styles/Actions.module.scss';
+import useStore from '../store/store';
 
 export default function Actions() {
+  const { invoices } = useStore();
+
   return (
     <div className={styles.actions}>
       <div className={styles.invoices}>
         <h2>Invoices</h2>
-        <p>7 invoices</p>
+        <p>{invoices.length} invoices</p>
       </div>
       <div className={styles.filter}>
         Filter <Image src={ArrowIcon} alt='arrow icon' />
