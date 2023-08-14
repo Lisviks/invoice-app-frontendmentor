50da791f4689ed767d4d704cba64a2c7f3962115
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-08-14 19:13:51 +0100

    interfaces.tsx file

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,34 +1,6 @@
 import { create } from 'zustand';
 import data from '@/data.json';
-
-interface Address {
-  street: string;
-  city: string;
-  postCode: string;
-  country: string;
-}
-
-interface Item {
-  name: string;
-  quantity: number;
-  price: number;
-  total: number;
-}
-
-interface Invoice {
-  id: string;
-  createdAt: string;
-  paymentDue: string;
-  description: string;
-  paymentTerms: number;
-  clientName: string;
-  clientEmail: string;
-  status: string;
-  senderAddress: Address;
-  clientAddress: Address;
-  items: Item[];
-  total: number;
-}
+import { Invoice } from '../interfaces';
 
 interface State {
   invoices: Invoice[];
