be9e6d6e535a936b435e6075bfadf18021d830e9
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-09-22 06:37:35 +0100

    EditInvoice component tablet screen styles

--- src/app/components/EditInvoice.tsx
+++ src/app/components/EditInvoice.tsx
@@ -14,7 +14,9 @@ interface Props {
 
 export default function EditInvoice({ isOpen, handleCloseEdit, invoice, newInvoice }: Props) {
   const [transalateX, setTransalateX] = useState('0');
+  const [backgroundOpacity, setBackgroundOpacity] = useState('0');
   const openingStyles = { transform: `translateX(${transalateX})` };
+  const backgroundOpeningStyles = { opacity: `${backgroundOpacity}` };
 
   const emptyInvoice: Invoice = {
     id: '',
@@ -43,28 +45,37 @@ export default function EditInvoice({ isOpen, handleCloseEdit, invoice, newInvoi
 
   const closeForm = () => {
     setTransalateX('0');
+    setBackgroundOpacity('0');
     handleCloseEdit();
   };
 
   useEffect(() => {
     isOpen ? setTransalateX('50rem') : setTransalateX('0');
+    isOpen ? setBackgroundOpacity('0.4984') : setBackgroundOpacity('0');
   }, [isOpen]);
 
   return (
-    <div className={styles.edit_invoice} style={isOpen ? openingStyles : {}}>
-      <div className={styles.go_back} onClick={() => closeForm()}>
-        <Image src={ArrowLeft} alt='arrow left' /> Go back
-      </div>
-      {newInvoice ? (
-        <div className={styles.edit_id}>New Invoice</div>
-      ) : (
-        <div className={styles.edit_id}>
-          Edit <span>#</span>
-          {invoice!.id}
+    <>
+      <div
+        className={styles.edit_invoice_outside_background}
+        style={isOpen ? backgroundOpeningStyles : {}}
+        onClick={closeForm}
+      ></div>
+      <div className={styles.edit_invoice} style={isOpen ? openingStyles : {}}>
+        <div className={styles.go_back} onClick={() => closeForm()}>
+          <Image src={ArrowLeft} alt='arrow left' /> Go back
         </div>
-      )}
+        {newInvoice ? (
+          <div className={styles.edit_id}>New Invoice</div>
+        ) : (
+          <div className={styles.edit_id}>
+            Edit <span>#</span>
+            {invoice!.id}
+          </div>
+        )}
 
-      <InvoiceForm values={invoice ? invoice : emptyInvoice} closeForm={closeForm} newInvoice={newInvoice} />
-    </div>
+        <InvoiceForm values={invoice ? invoice : emptyInvoice} closeForm={closeForm} newInvoice={newInvoice} />
+      </div>
+    </>
   );
 }
