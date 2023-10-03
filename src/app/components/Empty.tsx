39eb81b56acc4cfc040687f9a2c049ff4a381cf0
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-10-03 06:45:43 +0100

    change text on empty component based on screen width

--- src/app/components/Empty.tsx
+++ src/app/components/Empty.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import EmptyIllustration from '@/assets/illustration-empty.svg';
 import styles from '@/app/styles/Empty.module.scss';
+import useWindowWidth from '../hooks/useWindowSize';
 
 export default function Empty() {
+  const screenWidth = useWindowWidth();
+
   return (
     <div className={styles.empty}>
       <div>
@@ -10,7 +13,8 @@ export default function Empty() {
       </div>
       <h3>There is nothing here</h3>
       <p>
-        Create an invoice by clicking the <span>New</span> button and get started
+        Create an invoice by clicking the <span>{screenWidth < 768 ? 'New' : 'New Invoice'}</span> button and get
+        started
       </p>
     </div>
   );
